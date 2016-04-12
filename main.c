25903e0fbcc7463d394a2d966339820f01b941b3
Labelec-vm <Labelec-vm>
2016-04-12 17:08:52 +0200

    exo4 pas loin...

--- main.c
+++ main.c
@@ -10,9 +10,8 @@
 #include <sys/attribs.h>
 
 /* exo3
-void __ISR(_EXTERNAL_1_VECTOR, IPL3) btn (void)
+void __ISR(_EXTERNAL_1_VECTOR, IPL3) btn_ex3 (void)
 {
-        INTCONbits.INT1EP = 1 //front montant
         PR1 /= 2;
         if (PR1 < 3906 / 16)
             PR1 = 3906;
@@ -22,34 +21,78 @@ void __ISR(_EXTERNAL_1_VECTOR, IPL3) btn (void)
 */
 
 /*exo4*/
-void    dim(void)
-{
-    TMR1 = 0;
-}
+u8  dim_flag = 0;
+u8  dim_way = 0;
+u16 timer1_stamp = 0;
+u16 timer1_count = 0;
+u16 timer1_blink_period = 3906;
+u16 timer1_dim_period = 100;
+u16 timer1_default_period = 62500;
 
-void __ISR(_EXTERNAL_1_VECTOR, IPL3) btn (void)
-{
-    u8 start;
-    u8 end;
+u16 timer2_count = 0;
+u16 timer2_stamp = 0;
+u16 timer2_period_default = 62500;
 
-    start = 0;      // time stamp 1
-    INTCONbits.INT1EP = 1; //front montant
-    end = 0;        // time stamp 2
-    INTCONbits.INT1EP = 0; //front descendant
-    if ((end - start) > 2)
-        dim();
-    PR1 /= 2;
-    if (PR1 < 3906 / 16)
-        PR1 = 3906;
+void __ISR(_EXTERNAL_1_VECTOR, IPL3) btn_ex4 (void)
+{
+    u8  debounce = 0;
+
+    while (debounce < 5)
+        debounce++;
+
+    if (!INTCONbits.INT1EP)
+    {
+        timer2_stamp = TMR2;
+        INTCONbits.INT1EP = 1;
+        PR2 = 0;
+    }
+    else if (INTCONbits.INT1EP && TMR2 < timer2_period_default * 2
+            && !timer2_count)
+    {
+        dim_flag = 0;
+        PR1 /= 2;
+        if (PR1 < timer1_blink_period / 16)
+            PR1 = timer1_blink_period;
+    }
+    else
+    {
+        dim_flag = 1;
+    // Switch to dim
+    }
     IFS0bits.INT1IF = 0;
     TMR1 = 0;
 }
 
-void __ISR(_TIMER_1_VECTOR, IPL4) blink (void)
+void __ISR(_TIMER_1_VECTOR, IPL4) timer1_ex4 (void)
 {
     LATFbits.LATF1 = !LATFbits.LATF1;
     IFS0bits.T1IF = 0;
 }
+
+void __ISR(_TIMER_2_VECTOR, IPL5) timer2_ex4 (void)
+{
+    if (!dim_flag)
+        timer2_count++;
+    else
+    {
+        if (dim_way)
+            PR2++;
+        else
+            PR2--;
+        if (PR2 >= PR1)
+            dim_way = !dim_way;
+        LATFbits.LATF1 = !LATFbits.LATF1;
+    }
+    IFS0bits.T2IF = 0;
+}
+
+/*
+void __ISR(_TIMER_1_VECTOR, IPL4) blink_ex3 (void)
+{
+    LATFbits.LATF1 = !LATFbits.LATF1;
+    IFS0bits.T1IF = 0;
+}
+*/
 
 int main()
 {
@@ -159,7 +202,9 @@ void    exo4()
     IEC0bits.INT1IE = 1; // enable button Interrupt
     IPC1bits.INT1IP = 3; // priorite button interrupt
     IEC0bits.T1IE = 1; // enable Timer1 Interrupt
-    IPC1bits.T1IP = 4; // priorite button interrupt
+    IPC1bits.T1IP = 4; // priorite Timer1 interrupt
+    IEC0bits.T2IE = 1; // enable Timer2 Interrupt
+    IPC2bits.T2IP = 5; // priorite Timer2 interrupt
 
     __builtin_enable_interrupts(); // Tell CPU to look at interrupts
 
@@ -194,6 +239,13 @@ void    set_timer()
     T1CONbits.TCKPS = 0x7; // Valeur du prescaler (de 00 = 1:1 a 11 = 1:256)
     TMR1 = 0; // On reset la valeur du registre de comptage pour le timer 1
     T1CONbits.ON = 1; // On active le timer
+
+    T2CONbits.ON = 0; // On desactive le timer
+    T2CONbits.TGATE = 0; // Gated time accumulation desactive
+//    T2CONbits.TCS = 0; // On choisi comme source le TPBCLK
+    T2CONbits.TCKPS = 0x7; // Valeur du prescaler (de 00 = 1:1 a 11 = 1:256)
+    TMR2 = 0; // On reset la valeur du registre de comptage pour le timer 1
+    T2CONbits.ON = 1; // On active le timer
 }
 
 
